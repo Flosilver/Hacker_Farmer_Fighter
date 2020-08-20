4ae09460e99ae7d724f37e4a6c29d3202eaf8680
Flosilver <florentin.b98@gmail.com>
2020-08-20 13:39:20 +0200

    creation dossier serveur et client

---
+++ serveur/include/config.hpp
@@ -0,0 +1,20 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#include <Rosace.hpp>
+
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <ctime>
+#include <memory>
+#include <thread>
+
+using namespace std;
+using namespace rsc;
+
+#endif
---
+++ serveur/src/main.cpp
@@ -0,0 +1,5 @@
+#include "config.hpp"
+
+int main(){
+    cout << "Hello World!" << endl;
+}
